3403ef8ef39c520084303d3ff513fd8b099b1894
zvi <zvi.eliyahu@mail.huji.ac.il>
2025-08-03 12:57:57 +0300

    1

--- cleanData.py
+++ cleanData.py
@@ -1,2 +1,7 @@
-def cleanData():
-    pass
+import pandas as pd
+
+
+def cleanData(path:str):
+    presidential_speeches = pd.read_excel(path)
+    presidential_speeches = presidential_speeches.drop('Vice President', axis=1)
+    presidential_speeches.dropna(inplace=True)
--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import pandas as pd
-
+import cleanData
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    presidential_speeches = pd.read_excel('presidential_speeches.xlsx')
-    presidential_speeches = presidential_speeches.drop('Vice President', axis=1)
-    presidential_speeches.dropna(inplace=True)
-    cleaned_presidential_speeches =
+    cleaned_presidential_speeches =cleanData.cleanData('presidential_speeches.xlsx')
 
