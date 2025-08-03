42993f2b50504cc7daeddaac195792385c1c1544
zvi <zvi.eliyahu@mail.huji.ac.il>
2025-08-03 14:18:28 +0300

    1

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from cleanData import *
+from processData import *
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    cleaned_presidential_speeches = clean_presidential_speeches('presidential_speeches.xlsx')
-
+    cleaned_presidential_speeches = clean_presidential_speeches('data/presidential_speeches.xlsx')
--- processData.py
+++ processData.py
@@ -1,6 +1,12 @@
 import pandas as pd
 from textblob import TextBlob
+from transformers import pipeline
 
+labels = [
+    "Economy", "Foreign Policy", "National Security", "Civil Rights", "Healthcare",
+    "Education", "Environment", "Law and Order", "Government and Institutions",
+    "Technology and Innovation", "Religion and Morality", "Campaign and Politics"
+]
 # Extract sentiment per speech
 def extractSentiments(speech : str) -> float:
     blob = TextBlob(speech)
@@ -8,3 +14,10 @@ def extractSentiments(speech : str) -> float:
     # possibly process more
     return sentiment
 
+def extractTopic(speech:str,threshold)->list:
+    classifier = pipeline("zero-shot-classification", model="joeddav/xlm-roberta-large-xnli")
+    if not isinstance(speech, str) or speech.strip() == "":
+        return []
+    result = classifier(speech, candidate_labels=labels, multi_label=True)
+    tagged = [label for label, score in zip(result["labels"], result["scores"]) if score > threshold]
+    return tagged
