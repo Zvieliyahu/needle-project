222eb50aeb892cbf7f2e1810a7eda65f865f7a7a
zvi <zvi.eliyahu@mail.huji.ac.il>
2025-08-03 13:27:14 +0300

    1

--- cleanData.py
+++ cleanData.py
@@ -1,9 +1,11 @@
 import pandas as pd
 
-
 def cleanData(path:str):
     presidential_speeches = pd.read_excel(path)
+    #remove vice president column
     presidential_speeches = presidential_speeches.drop('Vice President', axis=1)
+    #remove rows with  None
     presidential_speeches.dropna(inplace=True)
+    #make speech and info lower case
     presidential_speeches['speech'] = presidential_speeches['speech'].str.lower()
     presidential_speeches['info'] = presidential_speeches['info'].str.lower()
