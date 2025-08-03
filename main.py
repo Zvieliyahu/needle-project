1608ca977f11ed47f2bbae1962807b702f40aa21
GilGil888 <giladmoriel@gmail.com>
2025-08-03 12:41:30 +0300

    2

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+import pandas as pd
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    presidential_speeches = pd.read_excel('presidential_speeches.xlsx')
+    cleaned_presidential_speeches =
 
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
