e5682c73833c50f8f8bc671509efb46d1679e42f
GilGil888 <giladmoriel@gmail.com>
2025-08-03 12:48:45 +0300

    2

--- main.py
+++ main.py
@@ -3,5 +3,7 @@ import pandas as pd
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     presidential_speeches = pd.read_excel('presidential_speeches.xlsx')
+    presidential_speeches = presidential_speeches.drop('Vice President', axis=1)
+    presidential_speeches.dropna(inplace=True)
     cleaned_presidential_speeches =
 
