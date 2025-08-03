38527599bb11fb9c039806e173505863ce76b6ea
ohad.gemer <ohadg99@gmail.com>
2025-08-03 13:34:55 +0300

    changed signature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd
-import cleanData
+from cleanData import *
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    cleaned_presidential_speeches =cleanData.cleanData('presidential_speeches.xlsx')
+    cleaned_presidential_speeches = clean_presidential_speeches('presidential_speeches.xlsx')
 
